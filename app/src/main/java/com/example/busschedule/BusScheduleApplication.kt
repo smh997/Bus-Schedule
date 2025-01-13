8c641acaae0bb14b08b35c2522cf6311e003709f
smh10 <smh10.mh@gmail.com>
2025-01-13 16:55:37 -0500

    Some clean ups.

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -1,7 +1,6 @@
 package com.example.busschedule
 
 import android.app.Application
-import android.content.Context
 import com.example.busschedule.data.BusScheduleDatabase
 
 class BusScheduleApplication : Application() {
